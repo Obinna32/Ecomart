1b665a0fefa8241e1f0d071671357fa0892fc20d
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-04-30 22:38:43 +0100

    feat: add product list view, urls, and html templates with media support

--- ecomart/urls.py
+++ ecomart/urls.py
@@ -21,9 +21,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include('shop.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
