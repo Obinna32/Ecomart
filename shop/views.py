73d459a6edc2323227f301f64ebb8c3327550488
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-04 14:14:22 +0100

    feat: add search functionality to product list view

--- shop/views.py
+++ shop/views.py
@@ -21,3 +21,23 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
 
     return render(request, 'shop/product/detail.html', {'product':product})
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    query = request.GET.get('query')
+    if query:
+        products = products.filter(name__icontains=query)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request, 'shop/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+        'query': query
+    })
