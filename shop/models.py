4943a894d6de325810ad5f01b6fbc5c7578067ad
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-04 12:13:43 +0100

    feat: implement the get_absolute_url for linking and updated detail HTML file

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
     
 
 class Product(models.Model):
@@ -35,3 +39,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
+    
