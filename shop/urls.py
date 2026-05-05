1d14e0acd7b9d4772659664a78e7de8dcbedc22d
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-05 23:06:36 +0100

    feat: add remove functionality to shopping cart

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('register/', views.register, name='register'),
     path('cart/add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
